bb40a807fee53ee98e0776dd7c8ca2a0cbadac89
adjeibad <dennis.adjei-baah@wwt.com>
2014-11-15 19:01:57 -0600

    Initial Commit

---
+++ FitFriends/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FitFriends
+//
+//  Created by Adjei-Baah, Dennis on 11/15/14.
+//  Copyright (c) 2014 Straight Arrow Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
